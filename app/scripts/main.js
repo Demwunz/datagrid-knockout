29720d65cab8e5ada94ea9ae8b6eaca8802ba419
Fazal Khan <faz888@gmail.com>
2013-08-14 19:37:57 +0100

    removing unneeded dependencies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,10 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery',
-        lodash : '../bower_components/lodash/lodash',
         knockout : '../bower_components/knockout/build/output/knockout-latest'
-    },
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
     }
 });
 
-require(['app', 'jquery', 'lodash', 'knockout'], function (app, $, _, ko) {
+require(['datagrid', 'knockout'], function (Datagrid, ko) {
     'use strict';
-    var dataGrid = new app();
-    console.log('Running jQuery %s', $().jquery);
+    var dataGrid = new Datagrid();
 });
